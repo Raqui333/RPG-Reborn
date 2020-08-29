2ae8841126b0aff0f71d8fc5ca2762b5a0936b34
UserUnavailable <klebersilva2025@gmail.com>
2020-08-29 17:43:48 -0300

    added battle animation

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,13 +1,19 @@
 #include "../include/enemy.h"
 
+// each rank has an int value E(2) D(3) C(4) B(5) A(6) S(7)
 Enemy::Enemy(const char *file, int init_hp, std::string name, int givenrank)
 {
     enemy_name = name;
+    
     enemy_texture = Texture::loadIMG(file);
+    SDL_SetTextureBlendMode(enemy_texture, SDL_BLENDMODE_BLEND);
+    
     battle_pos = {269, 71, 262, 262};
 
     hp = {init_hp, init_hp};
     rank = givenrank;
+
+    enemy_tick = SDL_GetTicks();
 }
 
 Enemy::~Enemy()
@@ -17,6 +23,7 @@ Enemy::~Enemy()
 
 void Enemy::render_in_battle()
 {
+    if (SDL_GetTicks() - enemy_tick > 100) SDL_SetTextureAlphaMod(enemy_texture, 255);
     SDL_RenderCopy(Game::renderer, enemy_texture, NULL, &battle_pos);
 }
 
@@ -32,6 +39,8 @@ std::pair<int,int> Enemy::gethp()
 
 void Enemy::dmg(int new_hp)
 {
+    SDL_SetTextureAlphaMod(enemy_texture, 0);
+    enemy_tick = SDL_GetTicks();
     hp.second -= (new_hp > 0) ? new_hp : 0;
 }
 
